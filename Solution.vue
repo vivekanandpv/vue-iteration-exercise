b1b69d6efa17a0f5484dafdd195c388fbddeb202
vivek <vivek@athenaeum.in>
2021-05-04 11:50:51 +0530

    done

---
+++ Solution.vue
@@ -0,0 +1,38 @@
+<template>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>First Name</th>
+        <th>Last Name</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="p in persons" :key="p.id">
+        <td>{{ p.id }}</td>
+        <td>{{ p.first_name }}</td>
+        <td>{{ p.last_name }}</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { persons } from "./data";
+export default {
+  data() {
+    return {
+      persons,
+    };
+  },
+};
+</script>
+
+<style>
+table,
+th,
+td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+</style>
---
+++ data.js
@@ -0,0 +1,54 @@
+//  refer: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/import
+
+export const persons = [
+  {
+    id: 1,
+    first_name: 'Dyane',
+    last_name: 'Benneton',
+  },
+  {
+    id: 2,
+    first_name: 'Luigi',
+    last_name: 'Francescuccio',
+  },
+  {
+    id: 3,
+    first_name: 'Amandi',
+    last_name: 'MacNeice',
+  },
+  {
+    id: 4,
+    first_name: 'Jard',
+    last_name: 'Trench',
+  },
+  {
+    id: 5,
+    first_name: 'Dominik',
+    last_name: 'Vinker',
+  },
+  {
+    id: 6,
+    first_name: 'Erma',
+    last_name: 'Fierman',
+  },
+  {
+    id: 7,
+    first_name: 'Kiley',
+    last_name: 'Goodered',
+  },
+  {
+    id: 8,
+    first_name: 'Calvin',
+    last_name: 'Verrell',
+  },
+  {
+    id: 9,
+    first_name: 'Janelle',
+    last_name: 'MacIver',
+  },
+  {
+    id: 10,
+    first_name: 'Grata',
+    last_name: 'Larroway',
+  },
+];
